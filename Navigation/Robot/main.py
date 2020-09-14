1ccf452fd84d3958224b995bdbc9d3fdefdb32ae
Jordan Shipard <jordanshipard@gmail.com>
2020-09-14 12:11:58 +1000

    Update main.py

--- Navigation/Robot/main.py
+++ Navigation/Robot/main.py
@@ -5,5 +5,5 @@ board = motorControl.initialiseBoard()
 start_time = time.time()
 while True:
     motorControl.SetTargetVelocities(board, 1, 0)
-    if start_time - time.time() == 10:
+    if time.time() - start_time == 10:
         break
