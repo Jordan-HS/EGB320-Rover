5ed4da834eaee9c4724387a5cb8009b42fc3d49c
BigGablonski <62833946+BigGablonski@users.noreply.github.com>
2020-10-01 11:16:27 +1000

    Update mobilityControl.py

--- mobilityControl.py
+++ mobilityControl.py
@@ -12,28 +12,45 @@ GPIO.setwarnings(False);
 pwmFreq = 100
 
 # Setup pins
-GPIO.setup(12, GPIO.OUT)    # PWMA
-GPIO.setup(18, GPIO.OUT)    # AIN2
-GPIO.setup(16, GPIO.OUT)    # AIN1
 GPIO.setup(22, GPIO.OUT)    # STBY
-GPIO.setup(15, GPIO.OUT)    # BIN1
-GPIO.setup(13, GPIO.OUT)    # BIN2
-GPIO.setup(11, GPIO.OUT)    # PWMB
 
-pwma = GPIO.PWM(12, pwmFreq)    # pin 18 to PWM
-pwmb = GPIO.PWM(11, pwmFreq)    # pin 13 to PWM
-pwma.start(100)
-pwmb.start(100)
+motorA_in1_pin = 16
+motorA_in2_pin = 18
+GPIO.setup(12, GPIO.OUT)    # PWMA
+GPIO.setup(motorA_in2_pin, GPIO.OUT)    # AIN2
+GPIO.setup(motorA_in1_pin, GPIO.OUT)    # AIN1
+motorA = GPIO.PWM(12, pwmFreq)    # pin 18 to PWM
+motorA.start(100)
+motorA.ChangeDutyCycle(0)
+
+motorB_in1_pin = 15
+motorB_in2_pin = 13
+GPIO.setup(motorB_in1_pin, GPIO.OUT)    # BIN1
+GPIO.setup(motorB_in2_pin, GPIO.OUT)    # BIN2
+GPIO.setup(11, GPIO.OUT)    # PWMB
+motorB = GPIO.PWM(11, pwmFreq)    # pin 13 to PWM
+motorB.start(100)
+motorA.ChangeDutyCycle(0)
 
 ## Functions
 #################################################################
-def forward(spd):
-    runMotor(0, spd, 0)
-    runMotor(1, spd, 0)
+def motorA_forward(spd):
+    GPIO.output(motorA_in1_pin, True)
+    GPIO.output(motorA_in2_pin, False)
 
-def reverse(spd):
-    runMotor(0, spd, 1)
-    runMotor(1, spd, 1)
+
+def motorA_reverse(spd):
+    GPIO.output(motorA_in1_pin, False)
+    GPIO.output(motorA_in2_pin, True)
+
+def motorB_forward(spd):
+    GPIO.output(motorB_in1_pin, True)
+    GPIO.output(motorB_in2_pin, False)
+
+def motorB_reverse(spd):
+    GPIO.output(motorB_in1_pin, False)
+    GPIO.output(motorB_in2_pin, True)
+
 
 def turnLeft(spd):
     runMotor(0, spd, 0)
@@ -68,25 +85,7 @@ def motorStop():
 #################################################################
 def main(args=None):
     while True:
-        forward(50)
-        sleep(2)
-        motorStop()
-        sleep(0.25)
-
-        reverse(50)
-        sleep(2)
-        motorStop()
-        sleep(0.25)
-
-        turnLeft(50)
-        sleep(2)
-        motorStop()
-        sleep(0.25)
-
-        turnRight(50)
-        sleep(2)
-        motorStop()
-        sleep(0.25)
+        motorA_forward()
 
 if __name__ == "__main__":
     main()
