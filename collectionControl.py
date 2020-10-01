014a4021f3855b0594f96888b7a28f5b2f4498c5
BigGablonski <62833946+BigGablonski@users.noreply.github.com>
2020-10-01 14:03:08 +1000

    Update collectionControl.py

--- collectionControl.py
+++ collectionControl.py
@@ -38,8 +38,8 @@ pwm.set_pwm_freq(60)
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    pwm.set_pwm(1, 0, servo_min)
+    pwm.set_pwm(0, 0, servo_centre)
+    pwm.set_pwm(1, 0, servo_centre)
     time.sleep(1)
     #pwm.set_pwm(0, 0, servo_max)
     #pwm.set_pwm(1, 0, servo_max)
