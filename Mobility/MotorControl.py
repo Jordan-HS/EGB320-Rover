889d3670e3839420e879c005eab5a840fc23960c
Jordan Shipard <jordanshipard@gmail.com>
2020-10-06 16:25:57 +1000

    Update MotorControl.py

--- Mobility/MotorControl.py
+++ Mobility/MotorControl.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     board.set_moter_pwm_frequency(1000)
 
     start = time.time()
-    duty = 5
+    duty = 6
     r = 0.01925
     while time.time() - start < 5:
         # for duty in range(5, 95, 10):   # slow to fast
