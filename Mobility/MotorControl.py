00510b70486c2ed61f25ccd82d110e43ee680cc1
Jordan Shipard <jordanshipard@gmail.com>
2020-10-06 16:05:08 +1000

    Update MotorControl.py

--- Mobility/MotorControl.py
+++ Mobility/MotorControl.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
                 # duty, speed[0], speed[1]))
 
         if speed[0] != 0 and speed[1] != 0:
-            print("speed: {:.2f}".format((2*math.pi*r)/((speed[0]+speed[1])/2)))
+            print("speed: {:.4f}".format((2*math.pi*r)/((speed[0]+speed[1])/2)))
 
         # # for duty in range(95, 5, - 10):   # fast to low
         #     # DC motor 1 movement, orientation clockwise
