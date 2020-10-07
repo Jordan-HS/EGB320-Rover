dc9edddf681580dd7fe2835f413110dd87075bf1
Jordan Shipard <jordanshipard@gmail.com>
2020-10-07 23:42:11 +1000

    Update encoder2.py

--- Mobility/encoder2.py
+++ Mobility/encoder2.py
@@ -95,10 +95,10 @@ def motorSetup():
     return board
 
 board = motorSetup()
-duty = 20
+duty = 40
 r = 0.01925
 start = time.time()
-while time.time() - start < 4:
+while time.time() - start < 6:
     forward(duty)
     
     time.sleep(.1)
