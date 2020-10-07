da1aa8992f910b35bf5317a02a30a1e91499195b
Jordan Shipard <jordanshipard@gmail.com>
2020-10-07 19:50:26 +1000

    Update MotorControl.py

--- Mobility/MotorControl.py
+++ Mobility/MotorControl.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
     
     time.sleep(2)
     start = time.time()
-    while time.time()-start<10.6:
+    while time.time()-start<10.7:
         try:
             # speed = board.get_encoder_speed(board.ALL)
             forward(duty)
