eb4de1911bd5f88999338c404cb7a49cf381953c
Jordan Shipard <jordanshipard@gmail.com>
2020-10-07 21:32:31 +1000

    up

--- Mobility/encoder.py
+++ Mobility/encoder.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
-import time, threading
+import time
+import threading
 
 panelNum = 1
 pinA = 23
 pinB = 24
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 error = 0
 counts = 0
 
-Encoder_A,Enconder_B = GPIO.input(pinA),GPIO.input(pinB)
+Encoder_A, Enconder_B = GPIO.input(pinA), GPIO.input(pinB)
 Encoder_B_old = GPIO.input(pinB)
 
-def reset(addr,tags,stuff,source):
+
+def reset(addr, tags, stuff, source):
 	global counts
 	global error
 	counts = 0
 	error = 0
 
+
 def encodercount(term):
-	global counts
-	global Encoder_A
-	global Encoder_B
-	global Encoder_B_old
-	global error
+    global counts
+    global Encoder_A
+    global Encoder_B
+    global Encoder_B_old
+    global error
 
-	Encoder_A,Encoder_B = GPIO.input(pinA),GPIO.input(pinB)
+    Encoder_A, Encoder_B = GPIO.input(pinA), GPIO.input(pinB)
 
-    if((Encoder_A,Encoder_B_old) == (1,0)) or ((Encoder_A,Encoder_B_old) == (0,1)):
+    if (((Encoder_A,Encoder_B_old) == (1,0)) or ((Encoder_A,Encoder_B_old) == (0,1))):
         counts += 1
     elif ((Encoder_A,Encoder_B_old) == (1,1)) or ((Encoder_A,Encoder_B_old) == (0,0)):
         counts -= 1
