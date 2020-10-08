e5a6a45cbaf93cfeb286b98a0f01045fe866bf73
Jordan Shipard <jordanshipard@gmail.com>
2020-10-08 21:09:52 +1000

    Update MotorControl.py

--- Mobility/MotorControl.py
+++ Mobility/MotorControl.py
@@ -134,7 +134,7 @@ duty = 20
 ang = 0
 r = 0.018559
 r2 = 0.137
-turnLeft(duty)
+forward(duty)
 time.sleep(1)
 ang = ( ( ( (abs(count_E1)/1200)*2*math.pi*r) + ( (abs(count_E2)/1200)*2*math.pi*r) ) / (r2*math.pi) ) * 180 
 if (count_E1 > 0) and (count_E2 < 0):
