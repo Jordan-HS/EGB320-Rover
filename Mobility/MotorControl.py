0f5bdcdb1d56b231be2e5d0dcabe6e63b07f70ba
Jordan Shipard <jordanshipard@gmail.com>
2020-10-08 17:41:06 +1000

    Update MotorControl.py

--- Mobility/MotorControl.py
+++ Mobility/MotorControl.py
@@ -75,7 +75,7 @@ def turnLeft(magnitude):
     board.motor_movement([board.M2], board.CCW, duty)
 
 def turnRight(magnitude):
-    board.motor_movement([board.M1], board.CW, duty)
+    board.motor_movement([board.M1], board.CCW, duty)
     board.motor_movement([board.M2], board.CW, duty)
 
 def forward(magnitude):
