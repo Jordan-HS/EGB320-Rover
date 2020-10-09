7ad9b30fffc1cc6fd32f3f41dd15a520f49da348
BigGablonski <62833946+BigGablonski@users.noreply.github.com>
2020-10-09 10:01:14 +1000

    Update closecollection.py

--- SampleCollection/closecollection.py
+++ SampleCollection/closecollection.py
@@ -16,7 +16,7 @@ pwm.set_pwm_freq(60)
 
 # Configure min and max servo pulse lengths
 servo_min = round(MINus/INCREus)  # Min pulse length out of 4096
-servo_max = round(MAXus/INCREus)-5  # Max pulse length out of 4096
+servo_max = round(MAXus/INCREus)  # Max pulse length out of 4096
 servo_centre = round(CENTREus/INCREus)
 
 while True:
