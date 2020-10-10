643bcc9e1f4d9098e197eb922104e0a7a4347557
Jordan Shipard <jordanshipard@gmail.com>
2020-10-10 23:42:25 +1000

    Update motor_testing.py

--- Mobility/motor_testing.py
+++ Mobility/motor_testing.py
@@ -15,12 +15,12 @@ while True:
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
-    # directionFlag = input("set motor direction: ")
-    # if directionFlag == "back": # if user types "back" change direction of motor
-        # Backward.on() # Sets Backward Direction pin on
-        # Forward.off() # Sets Backward Direction pin on
-    # else:
-    Backward.off() # Sets Backward Direction off
-    Forward.on()   # Sets Backward Direction pin on
-    # speedFlag = float(input("set speed (between 0-1000): ")) # Gets a number from the from the user
-    SpeedPWM.value = 500/1000 # Sets the duty cycle of the PWM between 0-1
+    directionFlag = input("set motor direction: ")
+    if directionFlag == "back": # if user types "back" change direction of motor
+        Backward.on() # Sets Backward Direction pin on
+        Forward.off() # Sets Backward Direction pin on
+    else:
+        Backward.off() # Sets Backward Direction off
+        Forward.on()   # Sets Backward Direction pin on
+    speedFlag = float(input("set speed (between 0-1000): ")) # Gets a number from the from the user
+    SpeedPWM.value = speedFlag/1000 # Sets the duty cycle of the PWM between 0-1
