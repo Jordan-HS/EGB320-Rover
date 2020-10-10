17b6035585e9ca1b7c139d34c2ecc10432501862
Jordan Shipard <jordanshipard@gmail.com>
2020-10-10 16:25:02 +1000

    Update mobilityControl.py

--- mobilityControl.py
+++ mobilityControl.py
@@ -23,14 +23,14 @@ motorA = GPIO.PWM(12, pwmFreq)    # pin 18 to PWM
 motorA.start(100)
 motorA.ChangeDutyCycle(0)
 
-motorB_in1_pin = 15
-motorB_in2_pin = 19
-GPIO.setup(motorB_in1_pin, GPIO.OUT)    # BIN1
-GPIO.setup(motorB_in2_pin, GPIO.OUT)    # BIN2
-GPIO.setup(11, GPIO.OUT)    # PWMB
-motorB = GPIO.PWM(11, pwmFreq)    # pin 13 to PWM
-motorB.start(100)
-motorB.ChangeDutyCycle(0)
+# motorB_in1_pin = 15
+# motorB_in2_pin = 19
+# GPIO.setup(motorB_in1_pin, GPIO.OUT)    # BIN1
+# GPIO.setup(motorB_in2_pin, GPIO.OUT)    # BIN2
+# GPIO.setup(11, GPIO.OUT)    # PWMB
+# motorB = GPIO.PWM(11, pwmFreq)    # pin 13 to PWM
+# motorB.start(100)
+# motorB.ChangeDutyCycle(0)
 
 ## Functions
 #################################################################
@@ -58,21 +58,17 @@ def motorStop():
 
 ## Main
 #################################################################
-def main(args=None):
-    while True:
-        GPIO.output(22, GPIO.HIGH)
-        motorA.ChangeDutyCycle(50)
-        motorA_forward()
-        sleep(1)
-        motorA_reverse()
-        sleep(1)
 
-        motorB.ChangeDutyCycle(50)
-        motorB_forward()
-        sleep(1)
-        motorB_reverse()
-        sleep(1)
+GPIO.output(22, GPIO.HIGH)
+motorA.ChangeDutyCycle(50)
+motorA_forward()
+sleep(1)
+# motorA_reverse()
+# sleep(1)
 
+# motorB.ChangeDutyCycle(50)
+# motorB_forward()
+# sleep(1)
+# motorB_reverse()
+# sleep(1)
 
-if __name__ == "__main__":
-    main()
