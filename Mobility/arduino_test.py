8ce805145607e81815cb7bab21e62aeef6123df9
Jordan Shipard <jordanshipard@gmail.com>
2020-10-11 15:53:45 +1000

    Update arduino_test.py

--- Mobility/arduino_test.py
+++ Mobility/arduino_test.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
     ser.flush()
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline()
-            print(line)
+        # if ser.in_waiting > 0:
+        line = ser.readline()
+        print(line)
