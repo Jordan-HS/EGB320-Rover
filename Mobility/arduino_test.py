106b83c7b68f9e9e45f47207556d10ddb45baef6
Jordan Shipard <jordanshipard@gmail.com>
2020-10-11 20:18:29 +1000

    Update arduino_test.py

--- Mobility/arduino_test.py
+++ Mobility/arduino_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import serial
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyS0', 9600, 8, 'N', 1, timeout=5)
     ser.flush()
     while True:
         # if ser.in_waiting > 0:
