88be8e4a4e8c0efb69396ad5ee8e05400af63856
Jordan Shipard <jordanshipard@gmail.com>
2020-10-13 13:50:32 +1000

    Update testing.py

--- Rover/testing.py
+++ Rover/testing.py
@@ -23,7 +23,7 @@ class Rover:
 rover = Rover()
 motorControl.sendCommand("clear")
 while True:
-    rover.move("left", 300)
+    rover.move("left", 100)
     motorControl.sendCommand("forward")
     print(motorControl.updatePosition(rover))
     # clear()
