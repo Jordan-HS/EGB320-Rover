3f72448316f6867cb591cc672cead16f56d62c18
Jordan Shipard <jordanshipard@gmail.com>
2020-10-13 14:34:24 +1000

    Update arduino_test.py

--- Mobility/arduino_test.py
+++ Mobility/arduino_test.py
@@ -3,6 +3,8 @@ import serial
 
 ser = serial.Serial('/dev/ttyS0', 9600, 8, 'N', 1, timeout=5)
 ser.flush()
+
+ser.write(str(0).encode('utf-8'))
 while True:
     # if ser.in_waiting > 0:
     line = ser.readline()
