90cd83c53a71ca77e9bed0ec5c423598930621c3
Jordan Shipard <jordanshipard@gmail.com>
2020-10-15 02:39:51 +1000

    Update setup.py

--- Navigation/Vrep/setup.py
+++ Navigation/Vrep/setup.py
@@ -7,13 +7,13 @@ def init_sim():
     sceneParameters = SceneParameters()
 
     #sceneParameters.obstacle0_StartingPosition = [0.3, -0.2]  # starting position of obstacle 0 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
-    sceneParameters.obstacle0_StartingPosition = [-0.6, -0.6]  # starting position of obstacle 0 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
-    sceneParameters.obstacle1_StartingPosition = None   # starting position of obstacle 1 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
+    sceneParameters.obstacle0_StartingPosition = [-0.2, -0.6]  # starting position of obstacle 0 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
+    sceneParameters.obstacle1_StartingPosition = [0.2, -0.6]   # starting position of obstacle 1 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
     sceneParameters.obstacle2_StartingPosition = None   # starting position of obstacle 2 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
 
     sceneParameters.sample0_StartingPosition = None  # starting position of obstacle 0 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
     #sceneParameters.sample0_StartingPosition = None  # starting position of obstacle 0 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
-    sceneParameters.sample1_StartingPosition = [-0.75, -0.75]   # starting position of obstacle 1 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
+    sceneParameters.sample1_StartingPosition = [0, -0.8]   # starting position of obstacle 1 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
     sceneParameters.sample2_StartingPosition = None   # starting position of obstacle 2 [x, y] (in metres), -1 if want to use current vrep position, or none if not wanted in the scene
 
 
