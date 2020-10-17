37474d6afc78aa52b01585f06e320f6782567c5c
Jordan Shipard <jordanshipard@gmail.com>
2020-10-17 14:48:17 +1000

    Update LED_test.py

--- Navigation/Robot/LED_test.py
+++ Navigation/Robot/LED_test.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep function from the time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(9, GPIO.OUT, initial=GPIO.LOW) 
 
 while True: # Run forever
-    GPIO.output(11, GPIO.HIGH) # Turn on
+    GPIO.output(9, GPIO.HIGH) # Turn on
     sleep(1)                  # Sleep for 1 second
-    GPIO.output(11, GPIO.LOW)  # Turn off
+    GPIO.output(9, GPIO.LOW)  # Turn off
     sleep(1)   
