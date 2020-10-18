2f343b88238ecc984c06bd2536a945f3ec8cccd3
Jordan Shipard <jordanshipard@gmail.com>
2020-10-18 19:49:36 +1000

    Update main.py

--- Mobility/main.py
+++ Mobility/main.py
@@ -13,7 +13,7 @@ test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
 def move(direction, magnitude):
     if direction == "forward":
         motors.motor1.setSpeed(-200)
-        motors.motor2.setSpeed(200)
+        motors.motor2.setSpeed(300)
 
 try:
     setup()
