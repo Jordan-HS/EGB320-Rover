231598a5bf70969425405abcf6cbdd2b8054db15
Jordan Shipard <jordanshipard@gmail.com>
2020-10-18 19:52:32 +1000

    Update main.py

--- Mobility/main.py
+++ Mobility/main.py
@@ -12,14 +12,20 @@ test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
 
 def move(direction, magnitude):
     if direction == "forward":
-        motors.motor1.setSpeed(-240)
-        motors.motor2.setSpeed(240)
+        motors.motor1.setSpeed(magnitude)
+        motors.motor2.setSpeed(magnitude)
+    elif direction == "left":
+        motors.motor1.setSpeed(-magnitude)
+        motors.motor2.setSpeed(magnitude)
+    elif direction == "right":
+        motors.motor1.setSpeed(magnitude)
+        motors.motor2.setSpeed(-magnitude)
 
 try:
     setup()
 
     while True:
-        move("forward", 200)
+        move("left", 200)
         time.sleep(0.005)
 
 except KeyboardInterrupt:
