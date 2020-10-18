37f68762b5774aed97a29e0b7ba199eb7ac7f936
Jordan Shipard <jordanshipard@gmail.com>
2020-10-19 04:30:34 +1000

    Update grade5.py

--- Rover/grade5.py
+++ Rover/grade5.py
@@ -192,7 +192,7 @@ class Rover():
             tiltdowncollection.down()
             closecollection.close()
 
-            if rock[0] > 0.25:
+            if rock[0] > 0.35:
                 speed = "normal"
                 accuracy = 10 
             else:
