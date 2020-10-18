259f5d607eda847462102a2501d3b93c9eab23e2
Jordan Shipard <jordanshipard@gmail.com>
2020-10-19 02:39:20 +1000

    Update grade5.py

--- Rover/grade5.py
+++ Rover/grade5.py
@@ -85,7 +85,7 @@ class Rover():
                 
 
             if sample[0] < 0.114 or self.at_target:
-                self.move("stop", 0)
+                self.move("stop", "stop")
                 self.at_target = True
                 return
 
@@ -136,7 +136,7 @@ class Rover():
                 tiltdowncollection.down()      
 
             if rock[0] < 0.135 or self.at_target:
-                self.move("stop", 0)
+                self.move("stop", "stop")
                 self.at_target = True
                 return
 
