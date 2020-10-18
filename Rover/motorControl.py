7d6c69324eaf85e0a1bf8fd3a03b8ed39b6383f1
Jordan Shipard <jordanshipard@gmail.com>
2020-10-18 13:38:08 +1000

    Update motorControl.py

--- Rover/motorControl.py
+++ Rover/motorControl.py
@@ -9,14 +9,14 @@ ser.flush()
 #Setup pins
 M1_back = gpiozero.OutputDevice(18) # On/Off output
 M1_fwd = gpiozero.OutputDevice(23) #On/Off output
-M1_PWM = gpiozero.PWMOutputDevice(24) # set up PWM pin
+M1_PWM = gpiozero.PWMOutputDevice(24, True, 0, 1000) # set up PWM pin
 
 STBY = gpiozero.OutputDevice(8)  # Standby pin
 STBY.on()
 
 M2_back = gpiozero.OutputDevice(25) # On/Off output
 M2_fwd = gpiozero.OutputDevice(12) #On/Off output
-M2_PWM = gpiozero.PWMOutputDevice(7) # set up PWM pin
+M2_PWM = gpiozero.PWMOutputDevice(7, True, 0, 1000) # set up PWM pin
 
 ## Parameters to adjust
 inner_turn_ratio = 1
@@ -30,12 +30,12 @@ def move(movement, magnitude=None):
         # Motor 1
         M1_back.on() 
         M1_fwd.off()
-        M1_PWM.value = magnitude*m1_motor_bias/1000
+        M1_PWM.value = 0.5
 
         # Motor 2
         M2_back.off()
         M2_fwd.on()
-        M2_PWM.value = magnitude*m2_motor_bias/1000
+        M2_PWM.value = 0.5
     elif movement == "left":
         # Motor 1
         M1_back.on() 
