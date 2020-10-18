263521dec76f9128478174d13597982f55f3b7aa
Jordan Shipard <jordanshipard@gmail.com>
2020-10-18 19:26:51 +1000

    upload

---
+++ Mobility/main.py
@@ -0,0 +1,22 @@
+import time
+from pololu_drv8835_rpi import motors, MAX_SPEED
+
+def setup():
+    motors.setSpeeds(0, 0)
+
+def close():
+    motors.setSpeeds(0, 0)
+
+def move(direction, magnitude):
+    if direction == "forward":
+        motors.motor1.setSpeed(magnitude)
+        motors.motor2.setSpeed(magnitude)
+
+try:
+    setup()
+
+    while True:
+        move("forward", 100)
+
+except KeyboardInterrupt:
+    close()
