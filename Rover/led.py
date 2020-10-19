00947595b796934d5917f68c9f19bf6e16150f1f
Jordan Shipard <jordanshipard@gmail.com>
2020-10-19 12:43:26 +1000

    Update led.py

--- Rover/led.py
+++ Rover/led.py
@@ -12,17 +12,17 @@ print("Testing RF out, Press CTRL+C to exit")
 
 def led_state(collection_stage):
    if collection_stage == "searching":
-      print("STATE: Searching")
+      # print("STATE: Searching")
       GPIO.output(16, GPIO.HIGH)
       GPIO.output(20, GPIO.LOW)
       GPIO.output(21, GPIO.LOW)
    elif collection_stage == "collecting":
-      print("STATE: Collecting")
+      # print("STATE: Collecting")
       GPIO.output(16, GPIO.LOW)
       GPIO.output(20, GPIO.HIGH)
       GPIO.output(21, GPIO.LOW)
    elif collection_stage == "SampleReturn":
-      print("STATE: Searching")
+      # print("STATE: Searching")
       GPIO.output(16, GPIO.LOW)
       GPIO.output(20, GPIO.LOW)
       GPIO.output(21, GPIO.HIGH)
